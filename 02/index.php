1d0233936c8a0295e18965c2ebc8fda0d874ad5e
ishpreetsidhu13 <ishpreetsidhu13@gmail.com>
2026-01-14 15:36:22 -0500

    add lines and code

--- 02/index.php
+++ 02/index.php
@@ -1,22 +1,37 @@
-//1. Set Up & Start 
+/1. Set Up & Start
 
 
-//2. Code Commenting 
+//2. Code Commenting
+
+// inline Comment
 
 <?php
 declare(strict_types=1);
 
-//3. Variables, Data Types, Concatenation, Conditional Statements & Echo
-$firstName = "John";
-$lastName = "Doe";
-$age = 30;
 
-echo "My name is " . $firstName . " " . $lastName . " and I am " . $age . " years old.";
+//3. Variables, Data Tyoes, Concatenation, Conditional Statements & Echo
+$firstName = "Harmanjeet"; // string
+$lastName = "Singh"; // string
+$age = 20; // int
+$isStudent = true; // boolean
 
+echo "<p>Hello there, my name is " . $firstName . " " . $lastName . "</p>";
+
+if ($isStudent) {
+    echo "<p>I am a student and I am " . $age . " years old.</p>";
+} else {
+    echo "<p> opps I am not a student.</p>";
+}
 //4. Loosely Typed Language Demo
-                
+$num1= 1;
+$num2= "10";
 
+function add(int $num1, int $num2) :int {
+    return $num1 + $num2;
+}
+echo "<p>" . add ($num1, $num2) ."</p>";
 //5. Strict Types & Types Hints
+declare(strict_types=1);
 
 
 //6. OOP with PHP 

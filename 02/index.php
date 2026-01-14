36c4566c293d09dec32d577a53650a8c953a81f8
ishpreetsidhu13 <ishpreetsidhu13@gmail.com>
2026-01-14 14:39:39 -0500

    add files

---
+++ 02/index.php
@@ -0,0 +1,16 @@
+//1. Set Up & Start 
+
+
+//2. Code Commenting 
+
+
+//3. Variables, Data Tyoes, Concatenation, Conditional Statements & Echo
+
+
+//4. Loosely Typed Language Demo
+
+
+//5. Strict Types & Types Hints
+
+
+//6. OOP with PHP 

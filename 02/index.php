91eedb934fb313bac263cc0bd7db7ba9ccb79d71
ishpreetsidhu13 <ishpreetsidhu13@gmail.com>
2026-01-14 15:29:04 -0500

    add files

--- 02/index.php
+++ 02/index.php
@@ -3,12 +3,18 @@
 
 //2. Code Commenting 
 
+<?php
+declare(strict_types=1);
 
-//3. Variables, Data Tyoes, Concatenation, Conditional Statements & Echo
+//3. Variables, Data Types, Concatenation, Conditional Statements & Echo
+$firstName = "John";
+$lastName = "Doe";
+$age = 30;
 
+echo "My name is " . $firstName . " " . $lastName . " and I am " . $age . " years old.";
 
 //4. Loosely Typed Language Demo
-
+                
 
 //5. Strict Types & Types Hints
 
